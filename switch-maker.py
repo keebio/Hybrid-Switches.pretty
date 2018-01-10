6cd5bff8df7b959279d5e78ab6d695e09f77d9e6
Danny Nguyen <danny@keeb.io>
2018-01-10 09:40:24 -0500

    Add creation of oval hybrid pads

--- switch-maker.py
+++ switch-maker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from KicadModTree import *
+import math
 
 class KeyboardSwitchMaker(object):
     """docstring for KeyboardSwitchMaker"""
@@ -24,11 +25,11 @@ class KeyboardSwitchMaker(object):
             'choc': (14.0, 14.0)
         }
         self.switch_spacing = 19.05
+        self.type_names = {'mx': 'MX', 'alps': 'Alps', 'choc': 'Choc'}
 
-    def make_switch(self, name, size):
+    def make_switch(self, name, size, sw_types):
         fp = Footprint(name)
         fp.setDescription('MX/Alps footprint')
-        sw_types = ['mx', 'alps']
 
         # Annotations
         fp.append(Text(type='reference', text='REF**', at=[0, 7.9375], layer='Dwgs.User'))
@@ -53,12 +54,63 @@ class KeyboardSwitchMaker(object):
         self.add_box(fp, width, height, 'Dwgs.User')
 
     def add_switch_pads(self, fp, sw_types):
-        for sw_type in sw_types:
-            pads = self.switch_pads[sw_type]
-            pad_size = 2.25
-            drill_size = 1.47
-            fp.append(Pad(number=1, type=Pad.TYPE_THT, shape=Pad.SHAPE_CIRCLE, at=pads[0], size=[pad_size, pad_size], drill=drill_size, layers=Pad.LAYERS_THT))
-            fp.append(Pad(number=2, type=Pad.TYPE_THT, shape=Pad.SHAPE_CIRCLE, at=pads[1], size=[pad_size, pad_size], drill=drill_size, layers=Pad.LAYERS_THT))
+        # Collect pads for combining
+        pad1_locations = [self.switch_pads[sw_type][0] for sw_type in sw_types]
+        pad2_locations = [self.switch_pads[sw_type][1] for sw_type in sw_types]
+
+        # Only one switch type
+        if len(sw_types) == 1:
+            for pad in pad1_locations:
+                self.add_normal_pad(fp, 1, pad)
+            for pad in pad2_locations:
+                self.add_normal_pad(fp, 2, pad)
+            return
+
+        # Add hybrid with first pad drilled
+        self.add_hybrid_pad(fp, 1, pad1_locations)
+        self.add_hybrid_pad(fp, 2, pad2_locations)
+
+        # Add remaining pads
+        for pad in pad1_locations[1:]:
+            self.add_normal_pad(fp, 1, pad)
+        for pad in pad2_locations[1:]:
+            self.add_normal_pad(fp, 2, pad)
+
+    def add_hybrid_pad(self, fp, pad_number, pads):
+        height = 2.25
+        drill_size = 1.47
+        width = self.pad_distance(pads) + height
+        location = self.oval_location(pads)
+        angle = self.oval_angle(pads)
+        offset = -self.pad_distance(pads)/2
+        print offset
+        fp.append(Pad(
+            number=pad_number,
+            type=Pad.TYPE_THT,
+            shape=Pad.SHAPE_OVAL,
+            at=pads[0],
+            rotation=angle,
+            size=[height, width],
+            offset=[0, offset],
+            drill=drill_size,
+            layers=Pad.LAYERS_THT))
+
+    def pad_distance(self, locations):
+        pad1, pad2 = locations[0:2]
+        return math.sqrt((pad1[0] - pad2[0]) ** 2 + (pad1[1] - pad2[1]) ** 2)
+
+    def oval_location(self, locations):
+        pad1, pad2 = locations[0:2]
+        return ((pad1[0] + pad2[0])/2, (pad1[1] + pad2[1])/2)
+
+    def oval_angle(self, locations):
+        pad1, pad2 = locations[0:2]
+        return math.degrees(math.atan2(pad1[0] - pad2[0], pad1[1] - pad2[1]))
+
+    def add_normal_pad(self, fp, pad_number, pad):
+        pad_size = 2.25
+        drill_size = 1.47
+        fp.append(Pad(number=pad_number, type=Pad.TYPE_THT, shape=Pad.SHAPE_CIRCLE, at=pad, size=[pad_size, pad_size], drill=drill_size, layers=Pad.LAYERS_THT))
 
     def add_led_pads(self, fp, sw_types):
         pad_size = 1.905
@@ -99,15 +151,17 @@ class KeyboardSwitchMaker(object):
     def add_stabilizers(self, fp):
         return
 
-
-    def oval_dimensions(pad1, pad2):
-        return {}
-
     def make_switches(self):
-        sizes = [1, 1.25]
+        sizes = [1]
+        hybrid_types = [
+            ['mx'],
+            ['mx', 'alps']
+        ]
         for size in sizes:
-            name = 'MX-Alps-{}u'.format(size)
-            self.make_switch(name, size)
+            for hybrid_type in hybrid_types:
+                hybrid_name = '-'.join(self.type_names[sw_type] for sw_type in hybrid_type)
+                name = '{}-{}u'.format(hybrid_name, size)
+                self.make_switch(name, size, hybrid_type)
 
 m = KeyboardSwitchMaker()
 m.make_switches()
